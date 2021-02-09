cb497b8b3d6c4c82b1cf436f5f745ecd5793f6e0
Domokos Tamas (RBRO\EPS3) <tamas.domokos@ro.bosch.com>
2021-02-09 09:06:16 +0200

    font change, store default vlaue change, dialog working

--- src/components/Dialog.svelte
+++ src/components/Dialog.svelte
@@ -1,8 +1,20 @@
 <script lang="ts">
     import TimeInput from "./TimeInput.svelte";
     import { workDuration } from "../store/store";
+    let duration: number = $workDuration;
+    let message: string = "";
+    function setNewWordDuration() {
+        workDuration.set(duration);
+        message = "New vaue has been set!";
+    }
 </script>
 
-<div class="d-flex justify-content-center p-2">
-    <TimeInput value={$workDuration} />
+<div class="d-flex flex-column">
+    <div class="d-flex justify-content-center p-2">
+        <TimeInput id="workduration" bind:value={duration} />
+    </div>
+    <div class="d-flex justify-content-center p-2">
+        <button class="btn btn-secondary ml-2 mr-2" on:click={() => setNewWordDuration()}> SAVE</button>
+    </div>
+    {message}
 </div>
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,15 +1,16 @@
-import { readable, writable, Writable } from "svelte/store";
+import { writable, Writable } from "svelte/store";
+const DEFAULT_TIME_MILIS: number = (8 * 60 + 30) * 60 * 1000;
 
 const storedPunchInTimeFromStorage: number = parseInt(localStorage.getItem("punchInTime"));
-const storedPunchInTime: number = isNaN(storedPunchInTimeFromStorage) ? Date.now() : storedPunchInTimeFromStorage;
+const storedPunchInTime: number = isNaN(storedPunchInTimeFromStorage) ? DEFAULT_TIME_MILIS : storedPunchInTimeFromStorage;
 export const punchInTime: Writable<number> = writable(storedPunchInTime);
-punchInTime.subscribe(value => {
+punchInTime.subscribe((value) => {
     localStorage.setItem("punchInTime", value.toString());
 });
 
 const storedWorkDurationFromStorage: number = parseInt(localStorage.getItem("workDuration"));
-const storedworkDuration: number = isNaN(storedWorkDurationFromStorage) ? (8 * 60 + 30) * 60 * 1000 : storedWorkDurationFromStorage;
+const storedworkDuration: number = isNaN(storedWorkDurationFromStorage) ? DEFAULT_TIME_MILIS : storedWorkDurationFromStorage;
 export const workDuration: Writable<number> = writable(storedworkDuration);
-workDuration.subscribe(value => {
+workDuration.subscribe((value) => {
     localStorage.setItem("workDuration", value.toString());
 });
